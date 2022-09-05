8f943e1fc5cebbc8897ea5ac5e6f6d92a9fee164
Hasan Baig <hasan4it@gmail.com>
2022-09-06 02:09:19 +0500

    Creating resource controller for publications, model and migrations

---
+++ app/Http/Controllers/PublicationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Publications;
+use Illuminate\Http\Request;
+
+class PublicationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Publications  $publications
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Publications $publications)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Publications  $publications
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Publications $publications)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Publications  $publications
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Publications $publications)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Publications  $publications
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Publications $publications)
+    {
+        //
+    }
+}
